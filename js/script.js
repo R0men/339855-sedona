b3378ad0630dc72c1331a54f369ef7a0ae786e0d
Roman <rom.konashenkov@yandex.ru>
2017-12-09 23:59:44 +0300

    Добавил  JS и анимацию

--- js/script.js
+++ js/script.js
@@ -1,8 +1,26 @@
 var formButton = document.querySelector(".button-form");
 var searchForm = document.querySelector(".search-tours-form");
+var inputArrival = searchForm.querySelector("[name=arrival]");
+var inputDeparture = searchForm.querySelector("[name=departure]");
+var inputAdults = searchForm.querySelector("[name=adults]");
+var inputChildren = searchForm.querySelector("[name=children]");
 
 formButton.addEventListener("click", function(evt) {
 	evt.preventDefault();
 	searchForm.classList.toggle("form-close");
+	if (!searchForm.classList.contains("form-close")) {
+		searchForm.classList.add("form-bounce");
+		inputArrival.focus();
+	};
 });
+
+searchForm.addEventListener("submit", function(evt) {
+ 	if (!inputArrival.value || !inputDeparture.value || !inputAdults.value || !inputChildren.value) {
+ 		evt.preventDefault();
+ 		searchForm.classList.add("form-error");
+ 	}
+ });
+
+
+
 
